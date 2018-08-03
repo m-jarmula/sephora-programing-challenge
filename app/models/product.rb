eef11cf3ca72d5fd7cb38a14fddd08ec0a926500
Tommi Lew <lew.tommi@gmail.com>
2018-08-03 16:20:20 +0800

    Add validation to Product’s category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :name, presence: true
+  validates :category, inclusion: { in: %w(makeup skincare hair) }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of(:name) }
+  it { expect(true).to be(true) }
+  it { is_expected.to validate_inclusion_of(:category).in_array(%w(makeup skincare hair)) }
 end
