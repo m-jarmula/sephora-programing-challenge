20505f1cc9b4695afacad67d4b46c545e7cf5683
Tommi Lew <lew.tommi@gmail.com>
2018-08-04 16:14:29 +0800

    Add sorting and filter to API V1 Products endpoint

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,8 +2,35 @@ module Api
   module V1
     class ProductsController < BaseController
       def index
-        everything = Product.select('*').order(id: :asc)
+        if params[:sort]
+          sorting = params[:sort]
+        else
+          sorting = nil
+        end
+
+        if sold_out = params.dig(:sold_out_filter)
+          sold_out = sold_out
+        else
+          sold_out = nil
+        end
+
+        everything = if sorting
+          Product.select('*').order(id: sorting.to_sym)
+        elsif sold_out
+          Product.select('*').where(sold_out: to_b(sold_out))
+        else
+          Product.select('*').order(id: :asc)
+        end
+
         render json: everything.to_json
+      end
+
+      def to_b(val)
+        if val == 'true'
+          return true
+        else
+          return false
+        end
       end
     end
   end
