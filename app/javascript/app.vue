71b2836a6bc5797afaf5f64f5dbca76ed1eed46c
Tommi Lew <lew.tommi@gmail.com>
2018-08-05 13:20:13 +0800

    Render Products data

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,47 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id='app'>
+    <h1 class='text-center'>{{ message }}</h1>
+    <div class='container-fluid'>
+      <div class='row' v-for='i in Math.ceil(products.length / productsPerRow)'>
+        <div class='col-sm' v-for='product in products.slice((i - 1) * productsPerRow, i * productsPerRow)'>
+          <span>{{ product.name }}</span>
+          <div class='text-center'>
+            <img src='http://via.placeholder.com/500x500' class='img-fluid'>
+          </div>
+          <span>S${{ product.price / 100 }}</span>
+          <div>Sold out: {{ product.sold_out }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: 'Happy Shop',
+      productsPerRow: 3,
+      products: null
     }
+  },
+
+  mounted () {
+    axios
+      .get('/api/v1/products')
+      .then(response => {
+        this.products = response.data
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
